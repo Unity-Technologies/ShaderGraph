3f8f7cf902d64099ba656eda375729c4d6416373
Peter Bay Bastian <peter.bastian@gmail.com>
2017-12-22 09:31:49 +0100

    Implement source mapping for node functions and use it to track errors

--- MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Util/FunctionRegistry.cs
+++ MaterialGraphProject/Assets/UnityShaderEditor/Editor/Data/Util/FunctionRegistry.cs
@@ -1,66 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Graphing;
 using UnityEngine;
 
 namespace UnityEditor.ShaderGraph
 {
-    class ShaderChunk
-    {
-        public INode node { get; set; }
-        public ShaderChunkType type { get; set; }
-        public string name { get; set; }
-        public int startLine { get; set; }
-        public int lineCount { get; set; }
-    }
-
-    enum ShaderChunkType
-    {
-        Function,
-        Body
-    }
-
     public class FunctionRegistry
     {
         Dictionary<string, string> m_Sources = new Dictionary<string, string>();
-        List<ShaderChunk> m_Chunks = new List<ShaderChunk>();
-        bool m_ValidationEnabled = false;
-        ShaderStringBuilder m_Builder = new ShaderStringBuilder();
-        const bool k_Validate = false;
+        bool m_Validate = false;
+        ShaderStringBuilder m_Builder;
 
-        public FunctionRegistry(int indentLevel = 0)
+        public FunctionRegistry(ShaderStringBuilder builder)
         {
+            m_Builder = builder;
         }
 
-        public INode currentNode { get; set; }
-
-        List<ShaderChunk> chunks
+        internal ShaderStringBuilder builder
         {
-            get { return m_Chunks; }
+            get { return m_Builder; }
         }
 
         public void ProvideFunction(string name, Action<ShaderStringBuilder> generator)
         {
-            m_Builder.AppendNewLine();
-            var startIndex = m_Builder.length;
-            generator(m_Builder);
-            var source = m_Builder.ToString();
-
             string existingSource;
             if (m_Sources.TryGetValue(name, out existingSource))
             {
-                if (k_Validate && source != existingSource)
-                    Debug.LogErrorFormat(@"Function `{0}` has varying implementations:{1}{1}{2}{1}{1}{3}", name, Environment.NewLine, source, existingSource);
+                if (m_Validate)
+                {
+                    var startIndex = builder.length;
+                    generator(builder);
+                    var length = builder.length - startIndex;
+                    var source = builder.ToString(startIndex, length);
+                    builder.length -= length;
+                    if (source != existingSource)
+                        Debug.LogErrorFormat(@"Function `{0}` has varying implementations:{1}{1}{2}{1}{1}{3}", name, Environment.NewLine, source, existingSource);
+                }
                 return;
             }
-            m_Sources.Add(name, source);
-            m_Chunks.Add(new ShaderChunk { node = currentNode, type = ShaderChunkType.Function, name = name, source = source });
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, m_Chunks.Select(f => f.source).ToArray());
+            {
+                builder.AppendNewLine();
+                var startIndex = builder.length;
+                generator(builder);
+                var length = builder.length - startIndex;
+                var source = m_Validate ? builder.ToString(startIndex, length) : string.Empty;
+                m_Sources.Add(name, source);
+            }
         }
     }
 }
